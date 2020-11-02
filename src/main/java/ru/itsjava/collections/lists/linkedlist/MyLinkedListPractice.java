507c2bff3c495c24b2cd6297f760b0ecd8e5632d
Vitaliy <SeekerOfConfessor@gmail.com>
2020-11-03 01:40:09 +0300

    add linked list

---
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPractice.java
@@ -0,0 +1,20 @@
+package ru.itsjava.collections.lists.linkedlist;
+
+public class MyLinkedListPractice {
+
+    public static void main(String[] args) {
+
+        MyLinkedList list = new MyLinkedList();
+
+        list.add("Строка 1");
+        System.out.println("list = " + list);
+
+        list.add("Строка 2");
+        System.out.println("list = " + list);
+
+        list.add("Строка 3");
+        System.out.println("list = " + list);
+
+
+    }
+}
